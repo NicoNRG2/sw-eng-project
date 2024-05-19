8026741ca24daa0189e7c8264b21ac03585bbc94
Leonardo Pasquato <leonardo.pasquato@gmail.com>
2024-05-19 17:50:35 +0200

    feature: added logo and indications, first draft button

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -12,15 +12,52 @@
           Come pick up your order at [Street Name]
         </v-col>
       </v-footer>
+
+      <v-row>
+        <!-- Left side with indications-->
+        <v-col cols="12" md="6">
+
+          <v-card class="elevation-10" color="brown darken-4" dark>
+            
+            <v-card-title class="white--text">
+              What you can find in Bakery4You:
+            </v-card-title>
+
+            <v-cart-text>
+              <ul class="white--text list-style-disc">
+
+                <li> Easy products consultation;</li>
+                <li> Always up-to-date products availability;</li>
+                <li> ...</li>
+              </ul>
+            </v-cart-text>
+          </v-card>
+
+
+          <v-btn color="brown darken-4" dark large>Get started</v-btn>
+
+        </v-col>
+
+        <!-- Right side with Bakery4You logo -->
+        <v.col cols="12" md="6" class="text-md-right">
+          <div>
+            <img :src="logo" alt="" style="max-width:30%; height: auto; margin-top: -125px">
+          </div>
+        </v.col>
+      </v-row>
+      
     </v-container>
   </v-app>
 </template>
 
 <script>
+import logo from '@/assets/logo.png';
+
 export default {
   name: 'App',
   data() {
     return {
+      logo,
       links: [
         { text: 'Gluten Free', route: '/gluten-free' },
         { text: 'Bread', route: '/bread' },
@@ -37,4 +74,25 @@ export default {
 
 <style>
 @import 'vuetify/styles';
+</style>
+
+<style>
+  .list-style-disc{
+    list-style-type: disc;
+  }
+</style>
+
+<style>
+.v-card {
+  width: 100%;
+  height: 100%;
+  max-width: 400px;
+  margin-left: -35px;
+  padding-left: 35px;
+  padding-top: -50px;
+  margin-top: 50px;
+
+  margin-bottom: 50px;
+
+}
 </style>
