aac7d8026301e496d243da2c057f323c2467dc25
AlessiaRinaldi <alessia.rinaldi@studenti.unitn.it>
2024-05-20 14:46:06 +0200

    chore: handle on Bakery4You

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -7,18 +7,18 @@
 
 // Composables
 import { createRouter, createWebHistory } from 'vue-router/auto'
-import Home from '../components/Home.vue';
+//import Home from '../components/Home.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
 })
 
 const routes = [
-  {
+  /*{
     path: '/',
     name: 'Home',
     component: Home,
-  },
+  },*/
   {
     path: '/gluten-free',
     name: 'GlutenFree',
