1a312c925c3e7dc0b975df5fac3e8b1bca2b9e07
Leonardo Pasquato <leonardo.pasquato@gmail.com>
2024-05-29 22:25:22 +0200

    feature: added upload image method

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const multer = require('multer');
 const productController = require('../controllers/productController');
 
 // Route to get all products
@@ -19,5 +20,43 @@ router.put('/:id', productController.updateProduct);
 
 // Route to delete a product
 router.delete('/:id', productController.deleteProduct);
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb){
+        cb(null, 'uploads/');
+    },
+    
+    filename: function (req, file, cb){
+        cb(null, Date.now() + '-' + file.originalname);
+    }
+});
+
+const upload = multer({
+    storage: storage
+});
+
+router.post('/:productId/upload-image', upload.single('image'), async (req, res) => {
+    const productId = req.params._id;
+    const imagePath = req.file.path;
+
+    try{
+        const product = await Product.findById(req.params.id);
+        if(!product){
+            return res.status(404).send('Product not found');
+        }
+
+        product.images.push(imagePath);
+        await product.save();
+
+        res.status(200).send({
+            message: 'Image uploaded successfully', product
+        });
+    
+    } catch(error){
+        res.status(500).send({
+            message: 'Error uploading image', error
+        });
+    }
+});
 
 module.exports = router;
