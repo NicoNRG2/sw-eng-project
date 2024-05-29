5c79801b9113d771978a0443d089551f448b76e0
NicoNRG2 <nicolacappellaro2@gmail.com>
2024-05-29 17:47:08 +0200

    fix: update shoppingCartRoutes

--- backend/routes/shoppingCartRoutes.js
+++ backend/routes/shoppingCartRoutes.js
@@ -1,14 +1,20 @@
 const express = require('express');
-const {
-  getCartById,
-  updateCart,
-  deleteCart
-} = require('../controllers/shoppingCartController');
-
 const router = express.Router();
+const shoppingCartController = require('../controllers/shoppingCartController');
 
-router.get('/:id', getCartById);
-router.put('/:id', updateCart);
-router.delete('/:id', deleteCart);
+// Route to add items to the shopping cart
+router.post('/add', shoppingCartController.addToCart);
+
+// Route to remove items from the shopping cart
+router.post('/remove', shoppingCartController.removeFromCart);
+
+// Route to create a new shopping cart
+router.post('/', shoppingCartController.createCart);
+
+// Route to update items in the shopping cart
+router.put('/:id', shoppingCartController.updateCart);
+
+// Route to get a shopping cart by user ID
+router.get('/user/:userId', shoppingCartController.getCartByUserId);
 
 module.exports = router;
